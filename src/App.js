996af0e115710f0dcbf77b6d7f908d6b85fbbc6b
ang92 <ang_92@list.ru>
2023-05-12 13:21:54 +0300

    fix conflicts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Button } from "./components/button/Button"
+import {Subtitle} from "./components/subtitle/Subtitle"
 
 function App() {
   return (
     <div className="App">
       <h1>hi</h1>
+      <Subtitle></Subtitle>
       <Button></Button>
     </div>
   );
