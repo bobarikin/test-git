5af2ac2fcf893f5aa561e9cb4862c25f9246aad9
Minakli <ges-a@ya.ru>
2023-05-12 15:26:01 +0530

    Header has been created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Button } from "./components/button/Button"
+import { Button } from "./components/button/Button";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <h1>hi</h1>
+      <Header />
       <Button></Button>
     </div>
   );
