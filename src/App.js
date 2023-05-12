99137a05afdf405a7b3d27f599e6ef0a7c98a26d
Ksusha <xiuschv@yandex.ru>
2023-05-12 12:58:00 +0600

    add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Button } from "./components/button/Button"
+import { Header } from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>hi</h1>
       <Button></Button>
     </div>
