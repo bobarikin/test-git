2ae155e57c9695a7bd6121f0492609ee43f449c6
ilia <iliajuso@gmail.com>
2023-05-12 14:07:34 +0400

    add Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Button } from "./components/button/Button"
-
+import {Footer} from "./components/button/Footer"
 function App() {
   return (
     <div className="App">
       <h1>hi</h1>
       <Button></Button>
+      <Footer/>
     </div>
   );
 }
