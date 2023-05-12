c00d4585a7cd44c2488b60545790ec7a82b3cd90
SergeyLoder <9790809@mail.ru>
2023-05-12 13:47:33 +0300

    add commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Button } from "./components/button/Button"
+import { Input } from "./components/Input/Input";
 
 function App() {
   return (
     <div className="App">
       <h1>hi</h1>
       <Button></Button>
+      <Input></Input>
     </div>
   );
 }
