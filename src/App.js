1d1bac82dec962dde1a0b6aa5d3a1242adb502b4
Viktor <midyukinv@mail.ru>
2023-05-12 14:12:23 +0300

    add Button hey

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Button } from "./components/button/Button"
 import { Input } from "./components/button/Input/Input";
 import { Header } from "./components/header/Header";
-
+import { Button } from "./components/button/Input/Button/Button";
 function App() {
   return (
     <div className="App">
       <Header/>
       <h1>hi</h1>
       <Button></Button>
-      <Input/>
+      <Input />
+      <Button/>
     </div>
   );
 }
